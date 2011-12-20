2ac8c61f97bb570d3072ce62107fc35d3ab45f9a
joshinator414@gmail.com <joshinator414@gmail.com@95872626-aa76-c4a3-9c31-0aabf3854c98>
2011-12-20 03:40:27 +0000

    able to grab a number from the comboBox1_SelectionChanged that displays the Name

--- code/WpfApplication1/MainWindow.xaml.cs
+++ code/WpfApplication1/MainWindow.xaml.cs
@@ -15,33 +15,49 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static uint _textPageNumber = 1;
         private readonly GoogleVoice voice = new GoogleVoice();
 
         public MainWindow()
         {
             InitializeComponent();
+            SendTab.IsEnabled = false;
+            MessageTab.IsEnabled = false;
+            ContactTab.IsEnabled = false;
+            
+
+        }
+
+        public List<DropDownList> fetchList()
+        {
+            List<DropDownList> objDLL = new List<DropDownList>();
+            foreach (var v in voice.GetContacts())
+            {
+                objDLL.Add(new DropDownList(){Item = v.Value.name, value = v.Value.displayNumber});
+            }
+            return objDLL;
+        }
+
+
+        public class DropDownList
+        {
+            public String Item { get; set; }
+            public String value { get; set; }
         }
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            string userName = UsernameField.Text;
-            string password = PasswordField.Password;
-                
-            try
-            {
-                LoginResult result = voice.Login(userName, password);
-                bool newResult = !result.RequiresRelogin;
-                MessageBox.Show(newResult.ToString());
-                TextTab.Focusable = true;
-                MessagesTab.Focusable = true;
-                closeTabItem(loginTab);
-            }
-            catch (Exception)
-            {
-                LoginError.Visibility = Visibility.Visible;
-                PasswordField.Clear();
-            }
+            string _userName = UsernameField.Text;
+            string _password = PasswordField.Password;
+            LoginResult result = voice.Login(_userName, _password);
+            bool newResult = !result.RequiresRelogin;
+            MessageBox.Show(newResult.ToString());
+            closeTabItem(loginTab);
+            SendTab.IsEnabled = true;
+            MessageTab.IsEnabled = true;
+            ContactTab.IsEnabled = true;
+            DestinationNumber.ItemsSource = fetchList();
+            DestinationNumber.DisplayMemberPath = "Item";
+            DestinationNumber.SelectedValuePath = "value";
         }
 
         private void closeTabItem(TabItem item)
@@ -58,113 +74,41 @@ namespace WpfApplication1
 
         private void SendTextButton_Click(object sender, RoutedEventArgs e)
         {
-            string phoneNr = DestinationNumber.Text;
+            string phoneNr = DestinationNumber.SelectedValue.ToString();
             string message = messageField.Text;
             bool isSent = voice.SMS(phoneNr, message);
-            MessageBox.Show(isSent.ToString());
+            if (isSent)
+            {
+            MessageBox.Show("Message sent :)");
+                
+            }
+            else
+            {
+                MessageBox.Show("Message not sent :(");
+            }
             messageField.Clear();
         }
 
-        private ObservableCollection<Contact> FilterResult = new ObservableCollection<Contact>();
 
         private void DestinationNumber_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            string key = DestinationNumber.Text;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
             foreach (var v in voice.GetContacts())
             {
-                if (v.Value.name.Contains(key) || v.Value.phoneNumber.Contains(key))
-                {
-                    FilterResult.Add(v.Value);
-                }
+                MessageBox.Show(v.Value.displayNumber);
             }
         }
 
-        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            updateTexts(voice.Texts(_textPageNumber).messages);
+            var lab = "Selected value " + DestinationNumber.SelectedValue;
+            label1.Content = lab;
         }
 
-        private void LastPageButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (_textPageNumber > 1)
-            {
-                _textPageNumber--;
-                updateTexts(voice.Texts(_textPageNumber).messages);
-            }
-        }
 
-        private void NextPageButton_Click(object sender, RoutedEventArgs e)
-        {
-            _textPageNumber++; //TODO: Check if at end page
-            updateTexts(voice.Texts(_textPageNumber).messages);
-        }
 
-        private void updateTexts(Dictionary<string, Message> messageDictionary)
-        {
-            var messages = new Message[messageDictionary.Count];
-            int counter = 0;
-            foreach (var message in messageDictionary)
-            {
-                messages[counter] = message.Value;
-                counter++;
-            }
-
-            //TODO: More efficient sort? It's 10 items max, so it doesn't really matter
-            for (int i = 0; i < messages.Length; i++)
-            {
-                for (int j = 0; j < messages.Length - 1; j++)
-                {
-                    if (messages[j].startTime < messages[j + 1].startTime)
-                    {
-                        var temp = messages[j];
-                        messages[j] = messages[j + 1];
-                        messages[j + 1] = temp;
-                    }
-                }
-            }
-
-            //TODO: Have this work according to how many texts are on the page.
-            //Current problem - don't know how to get it to choose which Grid, etc. to update if it's in a for loop. Ideally there wouldn't be a set number of TextXGrids, even though it will never be more than 10, but they would be added to the StackPanel as needed.
-            ContactNumber1.Content = messages[0].displayNumber;
-            Message1.Content = messages[0].messageText;
-            Text1Grid.Visibility = Visibility.Visible;
-
-            ContactNumber2.Content = messages[1].displayNumber;
-            Message2.Content = messages[1].messageText;
-            Text2Grid.Visibility = Visibility.Visible;
-
-            ContactNumber3.Content = messages[2].displayNumber;
-            Message3.Content = messages[2].messageText;
-            Text3Grid.Visibility = Visibility.Visible;
-
-            ContactNumber4.Content = messages[3].displayNumber;
-            Message4.Content = messages[3].messageText;
-            Text4Grid.Visibility = Visibility.Visible;
-
-            ContactNumber5.Content = messages[4].displayNumber;
-            Message5.Content = messages[4].messageText;
-            Text5Grid.Visibility = Visibility.Visible;
-
-            ContactNumber6.Content = messages[5].displayNumber;
-            Message6.Content = messages[5].messageText;
-            Text6Grid.Visibility = Visibility.Visible;
-
-            ContactNumber7.Content = messages[6].displayNumber;
-            Message7.Content = messages[6].messageText;
-            Text7Grid.Visibility = Visibility.Visible;
-
-            ContactNumber8.Content = messages[7].displayNumber;
-            Message8.Content = messages[7].messageText;
-            Text8Grid.Visibility = Visibility.Visible;
-
-            ContactNumber9.Content = messages[8].displayNumber;
-            Message9.Content = messages[8].messageText;
-            Text9Grid.Visibility = Visibility.Visible;
-
-            ContactNumber10.Content = messages[9].displayNumber;
-            Message10.Content = messages[9].messageText;
-            Text10Grid.Visibility = Visibility.Visible;
-
-        }
     }
 }

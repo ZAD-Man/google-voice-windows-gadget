dddc81384c05f690f0bfe8b2fd18ff5cc2664c65
The.ZAD.One@Gmail.com <The.ZAD.One@Gmail.com@95872626-aa76-c4a3-9c31-0aabf3854c98>
2011-12-20 01:14:35 +0000

    Displays texts if a full page is available. I don't know how to make it work with any number. Also made text and message tabs unavailable until the user is logged in. Also displays error message if wrong credentials are given.

--- code/WpfApplication1/MainWindow.xaml.cs
+++ code/WpfApplication1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows;
@@ -14,6 +15,7 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static uint _textPageNumber = 1;
         private readonly GoogleVoice voice = new GoogleVoice();
 
         public MainWindow()
@@ -23,12 +25,23 @@ namespace WpfApplication1
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            string _userName = UsernameField.Text;
-            string _password = PasswordField.Password;
-            LoginResult result = voice.Login(_userName, _password);
-            bool newResult = !result.RequiresRelogin;
-            MessageBox.Show(newResult.ToString());
-            closeTabItem(loginTab);
+            string userName = UsernameField.Text;
+            string password = PasswordField.Password;
+                
+            try
+            {
+                LoginResult result = voice.Login(userName, password);
+                bool newResult = !result.RequiresRelogin;
+                MessageBox.Show(newResult.ToString());
+                TextTab.Focusable = true;
+                MessagesTab.Focusable = true;
+                closeTabItem(loginTab);
+            }
+            catch (Exception)
+            {
+                LoginError.Visibility = Visibility.Visible;
+                PasswordField.Clear();
+            }
         }
 
         private void closeTabItem(TabItem item)
@@ -66,5 +79,92 @@ namespace WpfApplication1
             }
         }
 
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            updateTexts(voice.Texts(_textPageNumber).messages);
+        }
+
+        private void LastPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_textPageNumber > 1)
+            {
+                _textPageNumber--;
+                updateTexts(voice.Texts(_textPageNumber).messages);
+            }
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            _textPageNumber++; //TODO: Check if at end page
+            updateTexts(voice.Texts(_textPageNumber).messages);
+        }
+
+        private void updateTexts(Dictionary<string, Message> messageDictionary)
+        {
+            var messages = new Message[messageDictionary.Count];
+            int counter = 0;
+            foreach (var message in messageDictionary)
+            {
+                messages[counter] = message.Value;
+                counter++;
+            }
+
+            //TODO: More efficient sort? It's 10 items max, so it doesn't really matter
+            for (int i = 0; i < messages.Length; i++)
+            {
+                for (int j = 0; j < messages.Length - 1; j++)
+                {
+                    if (messages[j].startTime < messages[j + 1].startTime)
+                    {
+                        var temp = messages[j];
+                        messages[j] = messages[j + 1];
+                        messages[j + 1] = temp;
+                    }
+                }
+            }
+
+            //TODO: Have this work according to how many texts are on the page.
+            //Current problem - don't know how to get it to choose which Grid, etc. to update if it's in a for loop. Ideally there wouldn't be a set number of TextXGrids, even though it will never be more than 10, but they would be added to the StackPanel as needed.
+            ContactNumber1.Content = messages[0].displayNumber;
+            Message1.Content = messages[0].messageText;
+            Text1Grid.Visibility = Visibility.Visible;
+
+            ContactNumber2.Content = messages[1].displayNumber;
+            Message2.Content = messages[1].messageText;
+            Text2Grid.Visibility = Visibility.Visible;
+
+            ContactNumber3.Content = messages[2].displayNumber;
+            Message3.Content = messages[2].messageText;
+            Text3Grid.Visibility = Visibility.Visible;
+
+            ContactNumber4.Content = messages[3].displayNumber;
+            Message4.Content = messages[3].messageText;
+            Text4Grid.Visibility = Visibility.Visible;
+
+            ContactNumber5.Content = messages[4].displayNumber;
+            Message5.Content = messages[4].messageText;
+            Text5Grid.Visibility = Visibility.Visible;
+
+            ContactNumber6.Content = messages[5].displayNumber;
+            Message6.Content = messages[5].messageText;
+            Text6Grid.Visibility = Visibility.Visible;
+
+            ContactNumber7.Content = messages[6].displayNumber;
+            Message7.Content = messages[6].messageText;
+            Text7Grid.Visibility = Visibility.Visible;
+
+            ContactNumber8.Content = messages[7].displayNumber;
+            Message8.Content = messages[7].messageText;
+            Text8Grid.Visibility = Visibility.Visible;
+
+            ContactNumber9.Content = messages[8].displayNumber;
+            Message9.Content = messages[8].messageText;
+            Text9Grid.Visibility = Visibility.Visible;
+
+            ContactNumber10.Content = messages[9].displayNumber;
+            Message10.Content = messages[9].messageText;
+            Text10Grid.Visibility = Visibility.Visible;
+
+        }
     }
 }
